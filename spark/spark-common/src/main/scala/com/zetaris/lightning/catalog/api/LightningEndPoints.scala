6caccc181157771b5a18a6d57b7f69004726c7a9
Jaesung Jun <jason.jun@zetaris.com>
2025-01-07 15:56:21 +1100

    support local time stamp to serialize xml

--- spark/spark-common/src/main/scala/com/zetaris/lightning/catalog/api/LightningEndPoints.scala
+++ spark/spark-common/src/main/scala/com/zetaris/lightning/catalog/api/LightningEndPoints.scala
@@ -23,12 +23,26 @@ package com.zetaris.lightning.catalog.api
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.zetaris.lightning.execution.command.ShowDataQualityResult
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.catalyst.util.{DateFormatter, DefaultUDTUtils, SparkDateTimeUtils, TimestampFormatter, UDTUtils}
+import org.apache.spark.sql.internal.SqlApiConf
+import org.apache.spark.sql.types.{ArrayType, BinaryType, DataType, Decimal, MapType, StringType, StructType, UserDefinedType}
+import org.apache.spark.sql.{Row, SparkSQLBridge, SparkSession}
+import org.apache.spark.unsafe.types.CalendarInterval
+import org.apache.spark.util.SparkClassUtils
+import org.json4s.JsonAST.JValue
+import org.json4s.jackson.JsonMethods.compact
+import org.json4s.{JArray, JBool, JDecimal, JDouble, JField, JLong, JNull, JObject, JString}
 import org.slf4j.LoggerFactory
 
 import java.io.{BufferedWriter, OutputStream, OutputStreamWriter, PrintWriter, StringWriter}
+import java.sql.{Date, Timestamp}
+import java.time.{Instant, LocalDate, LocalDateTime}
+import java.util
+import java.util.Base64
 import javax.ws.rs._
 import javax.ws.rs.core.{MediaType, Response, StreamingOutput}
+import scala.collection.mutable
+import scala.util.control.NonFatal
 import scala.util.{Failure, Success, Try}
 
 // SparkSession Init
@@ -54,7 +68,7 @@ class LightningResource {
           val writer = new BufferedWriter(new OutputStreamWriter(output))
           writer.write("[")
           while(itr.hasNext) {
-            val json = itr.next().json
+            val json = rowToJson(itr.next())
             val jsonStr = mapper.writeValueAsString(json)
             if (itr.hasNext) {
               writer.write(s"$jsonStr,")
@@ -94,7 +108,7 @@ class LightningResource {
           var keepGoing = true
           writer.write("[")
           while(itr.hasNext && keepGoing) {
-            val json = itr.next().json
+            val json = rowToJson(itr.next())
             recCount += 1
             val jsonStr = mapper.writeValueAsString(json)
             if (itr.hasNext) {
@@ -136,7 +150,7 @@ class LightningResource {
         override def write(output: OutputStream): Unit = {
           val writer = new BufferedWriter(new OutputStreamWriter(output))
           while(itr.hasNext) {
-            val json = itr.next().json
+            val json = rowToJson(itr.next())
             recCount += 1
             val jsonStr = mapper.writeValueAsString(json)
             if (itr.hasNext) {
@@ -161,15 +175,84 @@ class LightningResource {
     val pw = new PrintWriter(sw)
     th.printStackTrace(pw)
 
-    val errorResponse = s"""{
-          "error": "Spark execution error",
-          "message": "${sw.toString}"
-        }"""
+    val errorMessage = new util.HashMap[String, String]()
+    errorMessage.put("error", "Spark execution error")
+    errorMessage.put("message", sw.toString)
 
+    val payload = new ObjectMapper().writeValueAsString(errorMessage)
     Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-      .entity(errorResponse)
+      .entity(payload)
       .build()
 
   }
 
+  // https://issues.apache.org/jira/browse/SPARK-50364
+  def rowToJson(row: Row): String = {
+    require(row.schema != null, "JSON serialization requires a non-null schema.")
+
+    lazy val zoneId = SparkDateTimeUtils.getZoneId(spark().sqlContext.getConf("spark.sql.session.timeZone"))
+    lazy val dateFormatter = DateFormatter()
+    lazy val timestampFormatter = TimestampFormatter(zoneId)
+
+    // Convert an iterator of values to a json array
+    def iteratorToJsonArray(iterator: Iterator[_], elementType: DataType): JArray = {
+      JArray(iterator.map(toJson(_, elementType)).toList)
+    }
+
+    // Convert a value to json.
+    def toJson(value: Any, dataType: DataType): JValue = (value, dataType) match {
+      case (null, _) => JNull
+      case (b: Boolean, _) => JBool(b)
+      case (b: Byte, _) => JLong(b)
+      case (s: Short, _) => JLong(s)
+      case (i: Int, _) => JLong(i)
+      case (l: Long, _) => JLong(l)
+      case (f: Float, _) => JDouble(f)
+      case (d: Double, _) => JDouble(d)
+      case (d: BigDecimal, _) => JDecimal(d)
+      case (d: java.math.BigDecimal, _) => JDecimal(d)
+      case (d: Decimal, _) => JDecimal(d.toBigDecimal)
+      case (s: String, _) => JString(s)
+      case (b: Array[Byte], BinaryType) =>
+        JString(Base64.getEncoder.encodeToString(b))
+      case (d: LocalDate, _) => JString(dateFormatter.format(d))
+      case (d: Date, _) => JString(dateFormatter.format(d))
+      case (i: Instant, _) => JString(timestampFormatter.format(i))
+      case (t: Timestamp, _) => JString(timestampFormatter.format(t))
+      case (d: LocalDateTime, _) => JString(timestampFormatter.format(d))
+      case (i: CalendarInterval, _) => JString(i.toString)
+      case (a: Array[_], ArrayType(elementType, _)) =>
+        iteratorToJsonArray(a.iterator, elementType)
+      case (a: mutable.ArraySeq[_], ArrayType(elementType, _)) =>
+        iteratorToJsonArray(a.iterator, elementType)
+      case (s: Seq[_], ArrayType(elementType, _)) =>
+        iteratorToJsonArray(s.iterator, elementType)
+      case (m: Map[String @unchecked, _], MapType(StringType, valueType, _)) =>
+        new JObject(m.toList.sortBy(_._1).map {
+          case (k, v) => k -> toJson(v, valueType)
+        })
+      case (m: Map[_, _], MapType(keyType, valueType, _)) =>
+        new JArray(m.iterator.map {
+          case (k, v) =>
+            new JObject("key" -> toJson(k, keyType) :: "value" -> toJson(v, valueType) :: Nil)
+        }.toList)
+      case (row: Row, schema: StructType) =>
+        var n = 0
+        val elements = new mutable.ListBuffer[JField]
+        val len = row.length
+        while (n < len) {
+          val field = schema(n)
+          elements += (field.name -> toJson(row(n), field.dataType))
+          n += 1
+        }
+        new JObject(elements.toList)
+      case (v: Any, udt: UserDefinedType[Any @unchecked]) =>
+        toJson(SparkSQLBridge.UDTUtils_toRow(v, udt), udt.sqlType)
+      case _ =>
+        throw new IllegalArgumentException(s"Failed to convert value $value " +
+          s"(class of ${value.getClass}}) with the type of $dataType to JSON.")
+    }
+    val jValue = toJson(row, row.schema)
+    compact(jValue)
+  }
 }
